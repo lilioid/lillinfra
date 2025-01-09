3277d0a112e70fc9d8c6f68e5d23f1ae4c0009af
lilly <li@lly.sh>
2025-01-09 20:24:42 +0100

    update user account name of rt-hosting.srv.ftsell.de

--- nix/systems/rt-hosting.srv.ftsell.de.nix
+++ nix/systems/rt-hosting.srv.ftsell.de.nix
@@ -52,8 +52,6 @@ in
 {
   imports = [
     ../modules/hosting_guest.nix
-    ../modules/base_system.nix
-    ../modules/user_ftsell.nix
   ];
 
   # boot config
@@ -477,6 +475,6 @@ in
 
   # DO NOT CHANGE
   # this defines the first version of NixOS that was installed on the machine so that programs with non-migratable data files are kept compatible
-  home-manager.users.ftsell.home.stateVersion = "24.05";
+  home-manager.users.lilly.home.stateVersion = "24.05";
   system.stateVersion = "24.05";
 }
