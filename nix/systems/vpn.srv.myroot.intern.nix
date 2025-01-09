c826b8719695253f4ad1b77c8dd5e207cf0fd2da
lilly <li@lly.sh>
2025-01-09 20:31:58 +0100

    rename user account on vpn.srv.myroot.intern

--- nix/systems/vpn.srv.myroot.intern.nix
+++ nix/systems/vpn.srv.myroot.intern.nix
@@ -12,8 +12,6 @@ in
 {
   imports = [
     ../modules/hosting_guest.nix
-    ../modules/base_system.nix
-    ../modules/user_ftsell.nix
   ];
 
   # filesystem mount config
@@ -151,6 +149,6 @@ in
 
   # DO NOT CHANGE
   # this defines the first version of NixOS that was installed on the machine so that programs with non-migratable data files are kept compatible
-  home-manager.users.ftsell.home.stateVersion = "24.05";
+  home-manager.users.lilly.home.stateVersion = "24.05";
   system.stateVersion = "24.05";
 }
