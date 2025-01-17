eb3251baeb2b9749fd0c5359843b959d7f8f1015
lilly <li@lly.sh>
2025-01-17 20:40:01 +0100

    fix autoUpgrade module not choosing the right nixosConfiguration output

--- nix/modules/auto_upgrade.nix
+++ nix/modules/auto_upgrade.nix
@@ -20,7 +20,7 @@ in
   config = lib.mkIf cfg.enable {
     system.autoUpgrade = {
       enable = true;
-      flake = "git+https://git.lly.sh/lilly/lillinfra.git";
+      flake = "git+https://git.lly.sh/lilly/lillinfra.git#${config.networking.fqdnOrHostName}";
       operation = if cfg.mode == "live" then "switch" else "boot";
       dates = "04:00";
       randomizedDelaySec = "30min";
