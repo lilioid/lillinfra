43f118fbd91bf761d68922d266eb6fe8b9e20a61
lilly <li@lly.sh>
2025-01-28 19:37:30 +0100

    wip: add private nix binary cache

--- nix/packages/default.nix
+++ nix/packages/default.nix
@@ -5,4 +5,5 @@
   installer = self.outputs.nixosConfigurations.installer.config.system.build.isoImage;
   show-nixos-diff = pkgs.callPackage ./show-nixos-diff { };
   harmonia-oci = pkgs.callPackage ./harmonia-oci.nix { harmonia = self.inputs.harmonia; };
+  openssh-oci = pkgs.callPackage ./openssh-oci.nix { };
 }
---
+++ nix/packages/openssh-oci.nix
@@ -0,0 +1,11 @@
+{ dockerTools, openssh }: dockerTools.buildLayeredImage {
+  name = "git.lly.sh/lilly/openssh";
+  tag = "latest";
+  config = {
+    Cmd = [ "${openssh}/bin/sshd" ];
+    ExposedPorts = {
+      "22/tcp" = {};
+    };
+  };
+}
+
