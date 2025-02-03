20ac3708f54831d9457f58f0a34f0348e72d75ea
lilly <li@lly.sh>
2025-02-03 18:20:00 +0100

    temporarily disable zfs for nas.srv.myroot.intern

--- nix/systems/nas.srv.myroot.intern.nix
+++ nix/systems/nas.srv.myroot.intern.nix
@@ -13,10 +13,10 @@
 
   # filesystem config (including zfs which adds additional mountpoints automatically)
   networking.hostId = "d1c39a07";
-  boot.supportedFilesystems = [ "zfs" ];
+  #boot.supportedFilesystems = [ "zfs" ];
   boot.zfs = {
     forceImportRoot = false;
-    extraPools = [ "hdd" "ssd" ];
+    #extraPools = [ "hdd" "ssd" ];
   };
   boot.loader.grub.device = "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0";
   fileSystems = {
