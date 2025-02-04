13c996d7b9965cd464996d76d49ca784eb9a374e
lilly <li@lly.sh>
2025-02-04 23:07:42 +0100

    fix kubernetes-dashboard access.sh script

--- k8s/apps/kubernetes-dashboard/access.sh
+++ k8s/apps/kubernetes-dashboard/access.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 set -eu
 
-TOKEN=$(kubectl --context=ftsell-de -n kubernetes-dashboard create token dashboard-user)
+TOKEN=$(kubectl --context=lly-sh -n kubernetes-dashboard create token dashboard-user)
 wl-copy "$TOKEN"
 
 echo "Url:        :  http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/"
 echo ""
 echo "Access Token (copied to clipboard):  $TOKEN"
 
-kubectl --context=ftsell-de proxy > /dev/null
+kubectl --context=lly-sh proxy > /dev/null
