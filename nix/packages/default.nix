91285db93917bbea316fa4fd6727604b245226c7
lilly <li@lly.sh>
2025-04-24 18:28:56 +0200

    remove unused nix package references

--- nix/packages/default.nix
+++ nix/packages/default.nix
@@ -4,8 +4,6 @@
   hetzner-ddns = pkgs.callPackage ./hetzner-ddns.nix { };
   installer = self.outputs.nixosConfigurations.installer.config.system.build.isoImage;
   show-nixos-diff = pkgs.callPackage ./show-nixos-diff { };
-  harmonia-oci = pkgs.callPackage ./harmonia-oci.nix { harmonia = self.inputs.harmonia; };
-  openssh-oci = pkgs.callPackage ./openssh-oci.nix { };
 
   # the kustomize-sops package installs itself as a library by default but we need it to be an executable in PATH
   kustomize-sops = pkgs.kustomize-sops.overrideAttrs (
