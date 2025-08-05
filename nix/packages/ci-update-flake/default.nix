297c2f6e7dab232026ba47b7c75a1f64a9adf48c
lilly <li@lly.sh>
2025-08-05 16:08:48 +0200

    restore update-flake ci from before custom script

--- nix/packages/ci-update-flake/default.nix
+++
@@ -1,9 +0,0 @@
-{ lib, pkgs, writeShellApplication }: writeShellApplication {
-  name = "ci-update-flake";
-  runtimeInputs = with pkgs; [
-    nix
-    git
-    curl
-  ];
-  text = builtins.readFile ./ci-update-flake.sh;
-}
