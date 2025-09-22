e68d457bc3da9d4d5ebcd680cd6b4d6645bdb14f
lilly <li@lly.sh>
2025-09-22 21:59:50 +0200

    configure database for db.z9.aut-num.de

--- nix/systems/db.z9.aut-num.de.nix
+++ nix/systems/db.z9.aut-num.de.nix
@@ -5,6 +5,29 @@
 
   custom.preset = "aut-sys-lxc";
 
+  # postgres config
+  systemd.services."postgresql".serviceConfig."Restart" = "on-failure";
+  services.postgresql = {
+    enable = true;
+    enableTCPIP = true;
+    ensureDatabases = [
+      "lilly"
+      "root"
+    ];
+    ensureUsers = [
+      {
+        name = "lilly";
+        ensureDBOwnership = true;
+        ensureClauses.superuser = true;
+      }
+      {
+        name = "root";
+        ensureDBOwnership = true;
+        ensureClauses.superuser = true;
+      }
+    ];
+  };
+
   # DO NOT CHANGE
   # this defines the first version of NixOS that was installed on the machine so that programs with non-migratable data files are kept compatible
   home-manager.users.lilly.home.stateVersion = "24.05";
