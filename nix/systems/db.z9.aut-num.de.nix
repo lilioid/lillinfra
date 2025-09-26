f516d459f137741baa8a5d7b600d04b8b9fc0b7d
lilly <li@lly.sh>
2025-09-26 15:25:24 +0200

    wip: aut-sys k8s setup

--- nix/systems/db.z9.aut-num.de.nix
+++ nix/systems/db.z9.aut-num.de.nix
@@ -5,6 +5,13 @@
 
   custom.preset = "aut-sys-lxc";
 
+  # allow connections from local network to postgres
+  networking.nftables.enable = true;
+  networking.firewall.extraInputRules = ''
+      ip saddr { 185.161.130.4 } tcp dport 5432 accept
+      ip6 saddr { 2a07:c481:2:5::/64, 2a07:c481:2:6::/64, 2a07:c481:2:7::/64 } tcp dport 5432 accept
+    '';
+
   # postgres config
   systemd.services."postgresql".serviceConfig."Restart" = "on-failure";
   services.postgresql = {
@@ -26,6 +33,12 @@
         ensureClauses.superuser = true;
       }
     ];
+    authentication = ''
+      host all all 2a07:c481:2:5::/64 scram-sha-256
+      host all all 2a07:c481:2:6::/64 scram-sha-256
+      host all all 2a07:c481:2:7::/64 scram-sha-256
+      host all all 185.161.130.0/28 scram-sha-256
+    '';
   };
 
   # DO NOT CHANGE
--- nix/systems/k8s-ctl.z9.aut-num.de.nix
+++ nix/systems/k8s-ctl.z9.aut-num.de.nix
@@ -1,25 +1,24 @@
-{
-  modulesPath,
-  config,
-  lib,
-  pkgs,
-  ...
+{ modulesPath
+, config
+, lib
+, pkgs
+, ...
 }: {
   custom.preset = "aut-sys-vm";
 
   # kubernetes setup
-#  services.k3s = {
-#    enable = true;
-#    role = "server";
-#    clusterInit = false;
-#    extraFlags = "--disable-helm-controller --disable=traefik --disable=servicelb --disable=local-storage --flannel-backend=vxlan --cluster-cidr 10.42.0.0/16 --service-cidr 10.43.0.0/16 --egress-selector-mode disabled --tls-san=k8s.lly.sh --node-taint node-role.kubernetes.io/control-plane=:NoSchedule";
-#    environmentFile = config.sops.secrets."k3s/secret.env".path;
-#  };
-#
-#  sops.secrets."k3s/secret.env" = {
-#    restartUnits = [ "k3s.service" ];
-#    key = "k3s/secretEnv";
-#  };
+  services.k3s = {
+    enable = true;
+    role = "server";
+    clusterInit = false;
+    extraFlags = "--disable-helm-controller --disable=traefik --disable=servicelb --disable=local-storage --flannel-backend=vxlan --cluster-cidr 10.42.0.0/16 --service-cidr 10.43.0.0/16 --egress-selector-mode disabled --tls-san=k8s.lly.sh --node-taint node-role.kubernetes.io/control-plane=:NoSchedule";
+    environmentFile = config.sops.secrets."k3s/secret.env".path;
+  };
+
+  sops.secrets."k3s/secret.env" = {
+    restartUnits = [ "k3s.service" ];
+    key = "k3s/secretEnv";
+  };
 
   # DO NOT CHANGE
   # this defines the first version of NixOS that was installed on the machine so that programs with non-migratable data files are kept compatible
