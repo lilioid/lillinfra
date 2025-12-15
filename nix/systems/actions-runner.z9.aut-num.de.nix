214428da46e69552a5179b0707a8e64a9250cd9e
lilly <li@lly.sh>
2025-12-15 21:40:33 +0100

    setup forgejo-actions-runner system

--- nix/systems/actions-runner.z9.aut-num.de.nix
+++ nix/systems/actions-runner.z9.aut-num.de.nix
@@ -1,5 +1,39 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, lib, ... }: {
   custom.preset = "aut-sys-vm";
+
+  networking.nameservers = [ "2620:fe::fe" ];
+
+  # setup runner
+  virtualisation.docker = {
+    enable = true;
+    autoPrune.enable = true;
+  };
+  systemd.services."gitea-actions-runner".description = lib.mkForce "Forgejo Actions Runner";
+  services.gitea-actions-runner = {
+    package = pkgs.forgejo-runner;
+    instances."git.hanse.de" = {
+      enable = true;
+      name = "aut-sys-runner";
+      tokenFile = config.sops.templates."forgejo-actions-runner/tokenFile".path;
+      url = "https://git.hanse.de/";
+      labels = [
+        "debian-latest:docker://node:current"
+        "alpine-latest:docker://node:current-alpine"
+        "nixos-latest:docker://nixos/nix"
+      ];
+      settings = {
+        container = {
+          docker_host = "automount";
+        };
+      };
+    };
+  };
+
+  # provide registration token to runner
+  sops.secrets."forgejo-actions-runner/registration-token" = {};
+  sops.templates."forgejo-actions-runner/tokenFile".content = ''
+    TOKEN=${config.sops.placeholder."forgejo-actions-runner/registration-token"}
+  '';
 
   # DO NOT CHANGE
   # this defines the first version of NixOS that was installed on the machine so that programs with non-migratable data files are kept compatible
