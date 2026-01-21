a6c6d014a1805a155fedbc904774c2b62e2c19ff
lilly <li@lly.sh>
2026-01-21 14:12:10 +0100

    remove hosting-pve opentofu module

--- tofu/hosting-pve/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "tenants" {
-  description = "List of tenants which have stuff running on my proxmox server"
-  type = list(object({
-    name = string
-    id   = number
-    vms = list(number)
-  }))
-}
-
