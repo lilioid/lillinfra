250027dbe4cb708388ed11b34652e3d530c35790
lilly <li@lly.sh>
2026-01-22 12:53:19 +0100

    remove proxmox opentofu variables

--- tofu/variables.tf
+++ tofu/variables.tf
@@ -1,13 +1,3 @@
-variable "proxmox_user" {
-  description = "username for proxmox authentication"
-  default     = "tofu@pve"
-}
-
-variable "proxmox_password" {
-  description = "password for proxmox authentication"
-  sensitive   = true
-}
-
 variable "desec_token" {
   description = "Auth-Token for desec.io"
   sensitive   = true
