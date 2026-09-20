1c1247af1b12f8db44cd9a28dba9d6604516dc05
lilly <li@lly.sh>
2026-09-20 20:37:29 +0200

    set webhost to empty VM (temporarily)

--- nix/systems/webhost.z9.aut-num.de.nix
+++ nix/systems/webhost.z9.aut-num.de.nix
@@ -1,31 +1,31 @@
 { pkgs, lib, config, ... }: {
-  custom.preset = "aut-sys-lxc";
+  custom.preset = "aut-sys-vm";
 
-  security.acme.acceptTerms = true;
-  security.acme.defaults.email = "li@lly.sh";
+  # security.acme.acceptTerms = true;
+  # security.acme.defaults.email = "li@lly.sh";
 
-  custom.webhosting = {
-    enable = true;
-    users.skye = {
-      domains = [ "lihesys.de" ];
-      sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC2vP9rQP6f6o61VUssBFvgY+O2sZ7T4OGaNkJTAk8G2 skye";
-    };
-    users.lilly = {
-      shell = pkgs.fish;
-      sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPzGnNKyn6jmVxig4SRnTBfpi6okPU2aOHPwFnAPTxJm ftsell@ftsell.de";
-    };
-  };
+  # custom.webhosting = {
+  #   enable = true;
+  #   users.skye = {
+  #     domains = [ "lihesys.de" ];
+  #     sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC2vP9rQP6f6o61VUssBFvgY+O2sZ7T4OGaNkJTAk8G2 skye";
+  #   };
+  #   users.lilly = {
+  #     shell = pkgs.fish;
+  #     sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPzGnNKyn6jmVxig4SRnTBfpi6okPU2aOHPwFnAPTxJm ftsell@ftsell.de";
+  #   };
+  # };
 
-  custom.backup = {
-    enable = true;
-    backupDirectories = lib.map
-      (i: "/home/${i}")
-      (lib.attrNames config.custom.webhosting.users);
-    destinations."rsync.net".path = "ssh://zh4525@zh4525.rsync.net/./backups/borg-repo";
-  };
+  # custom.backup = {
+  #   enable = true;
+  #   backupDirectories = lib.map
+  #     (i: "/home/${i}")
+  #     (lib.attrNames config.custom.webhosting.users);
+  #   destinations."rsync.net".path = "ssh://zh4525@zh4525.rsync.net/./backups/borg-repo";
+  # };
 
   # DO NOT CHANGE
   # this defines the first version of NixOS that was installed on the machine so that programs with non-migratable data files are kept compatible
-  home-manager.users.lilly.home.stateVersion = "24.05";
-  system.stateVersion = "24.05";
+  home-manager.users.lilly.home.stateVersion = "26.05";
+  system.stateVersion = "26.05";
 }
